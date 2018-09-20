9b5cb8da896166edad90720421add853566cc07a
earine <marinalunts@gmail.com>
2018-09-20 19:23:36 +0300

    side menu added

--- ShineExpert/LoginViewController.swift
+++ ShineExpert/LoginViewController.swift
@@ -24,5 +24,8 @@ class LoginViewController: UIViewController {
         
     }
 
-
+    // убирается клавиатура при нажатии в любой точке экрана
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 }
